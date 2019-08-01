348bae18307ac4f12dc864f884910ed25490c867
IrfSuar <iasl96@msn.com>
2019-07-31 22:03:34 -0500

    number two only ex

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const NAME = 'Irving'
+//What's the cyrcle area wich diamter is in 10cm ?
+
+const DIAMETERCIRCLE=10;
+var radiusCircle = DIAMETERCIRCLE/2;
+const PINUMBER=3.1416;
+console.log (PINUMBER*(radiusCircle**2))
+
+//the answer was using var const 
+
+//
+
+
+//Dado un número {number}, verificar si es par o non. evaluar que el número sí sea dígito
+var numberX=11
+
+    if (numberX%2 !=== 0) {
+        console.log ('Es non')
+    } else if (numberX%2 ===0){
+        console.log ('es par')}
+
+//Saber sí el producto de dos números es primo y mayor a cuarenta 
+//Validar que los numeros sean digitos
+//{numberOne} {numberTwo}
+/*Números primos son aquellos que se tienen que dividir entre sí mismos y uno
+*Para validarlos únicamente tenemos que hacer es (n-1)! = -1
+* para ver todas mis notas se pueden ver aquí https://1drv.ms/u/s!Atg0mPTln-WXgYdgopeTUoT94nvd-Q
